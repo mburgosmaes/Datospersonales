b1441e88322e2521d5eb006248b66bcdd16c5cc9
mburgomaes <mburgomaes@gmail.com>
2018-11-07 12:42:46 +0100

    Para entregar

--- app/src/main/java/com/example/mario/aplicacioncliente/MainActivity.java
+++ app/src/main/java/com/example/mario/aplicacioncliente/MainActivity.java
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     EditText lblApellidos;
     EditText lblEdad;
     Spinner lista;
+    RadioGroup btnGrup;
+    Switch switchHijo;
+    Button btnEjemplo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
         lblApellidos = findViewById(R.id.lblApellidos);
         lblEdad = findViewById(R.id.lblEdad);
         lista = (Spinner) findViewById(R.id.lista);
+        btnGrup = findViewById(R.id.btngrup);
+        switchHijo = findViewById(R.id.switchHijo);
+
+
+        //Switch
+        if(switchHijo.isChecked()){
+            //textView.setText("Sí");
+        }else{
+            //textView.setText("No");
+        }
 
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.datosCiviles, android.R.layout.simple_spinner_item);
@@ -42,9 +58,22 @@ public class MainActivity extends AppCompatActivity {
                 lblNombre.setText("");
                 lblApellidos.setText("");
                 lblEdad.setText("");
-
+                btnGrup.clearCheck();
+                lista.setSelection(0);
+                switchHijo.setChecked(false);
             }
         });
-    }
 
+        //Generar campos
+      /*  Button btnEjemplo = findViewById(R.id.btnEjemplo);
+        btnEjemplo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {txtinfo.getText(generar());}
+
+                public String generar() {
+                    String resultado = lblNombre.getText().toString() + ",";
+                    return resultado;
+                }
+    });*/
+    }
 }
